b12ddabfe2cbf94f1024168b70abb121bd190cdf
Pedro Nekaka <pedro17pedroo@gmail.com>
2021-04-07 22:37:59 +0100

    alterar o ficheiro config

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 const config = {
-    dbUrl: process.env.DB_URL || 'mongodb+srv://joseticnes_user:v51BdAz0BatlU7xR@cluster0.rjqps.mongodb.net/cbaDataBase?retryWrites=true&w=majority',
-    port: process.env.PORT || 3000,
-    host: process.env.HOST || 'http://localhost',
-    publicRoute: process.env.PUBLIC_ROUTER || '/app',
-    filesRoute:process.env.FILES_ROUTE || 'files'
+    dbUrl: process.env.DB_URL,
+    port: process.env.PORT,
+    host: process.env.HOST,
+    publicRoute: process.env.PUBLIC_ROUTER,
+    filesRoute:process.env.FILES_ROUTE
 
 }
 
